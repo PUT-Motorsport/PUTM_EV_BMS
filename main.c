4a42fd5a2f6b65bb972c445ae19824e6b09d0979
Morgaliel <maksymilian.jaruga@student.put.poznan.pl>
2020-10-02 22:53:40 +0200

    Big update
    I had to update almost every function because I forgot about 1 in every command. I read about LTC6810-1 and used LTC6810-2 so the mistake caused errors in SPI communication. Furthermore, I started to write some Interupt function to handle all the errors.
    The update also contains read from B group, which is almost the same as A but I hadn't written those earlier.

--- main.c
+++ main.c
@@ -1,49 +1,82 @@
+/*
+ * bms_1.c
+ *
+ * Created: 09/25/2020 20:46:55
+ * Author : Maks
+ */ 
+
 #include <avr/io.h>
-#define F_CPU 16000000
-#define SPI_CS_PIN 8
+#include <string.h>
 
+#define F_CPU 16000000UL
 
-#define RESET 0
-#define SET 1
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
+//	stan licznika timera
+volatile uint64_t timer_counter = 0;
+
+/*
 float temperature_map[29][2] = {
 	//    ltc value ,  temperature *C
-	27190 , -20 ,
-	26380 , -15 ,
-	25400 , -10 ,
-	24260 , -5  ,
-	22960 , 0 ,
-	21520 , 5 ,
-	19960 , 10  ,
-	18330 , 15  ,
-	16660 , 20  ,
-	15000 , 25  ,
-	13380 , 30  ,
-	11850 , 35  ,
-	10420 , 40  ,
-	9120  , 45  ,
-	7940  , 50  ,
-	6890  , 55  ,
-	5970  , 60  ,
-	5170  , 65  ,
-	4470  , 70  ,
-	3860  , 75  ,
-	3350  , 80  ,
-	2900  , 85  ,
-	2520  , 90  ,
-	2190  , 95  ,
-	1910  , 100 ,
-	1660  , 105 ,
-	1450  , 110 ,
-	1270  , 115
-
-};
+	48656 , -20 ,
+	45988 , -15 ,
+	44280 , -10 ,
+	42292 , -5  ,
+	40026 , 0 ,
+	37515 , 5 ,
+	34796 , 10  ,
+	31955 , 15  ,
+	29043 , 20  ,
+	26150 , 25  ,
+	23325 , 30  ,
+	20658 , 35  ,
+	18165 , 40  ,
+	15899  , 45  ,
+	13842  , 50  ,
+	12011  , 55  ,
+	10407  , 60  ,
+	9012  , 65  ,
+	7793  , 70  ,
+	6729  , 75  ,
+	5840  , 80  ,
+	5056  , 85  ,
+	4393  , 90  ,
+	3818  , 95  ,
+	3330  , 100 ,
+	2894  , 105 ,
+	2528  , 110 ,
+	2214  , 115
+};*/
 
 uint8_t ltc_config[6] = {0xFC, (1874 & 0xff), (1874>>4)|(2625<<4), (2625>>4), 0, 0};
 uint16_t cell_values[10];
+uint16_t temp_values[10];
 
-uint16_t temp_values[5];
-float temp_values_float[5];
+ISR(TIMER0_COMP_vect)
+{
+	TCNT0 = 0;
+	//timer_counter++;
+	
+		
+	
+	
+	//if(timer_counter >= DRS_MAX_TYPE_CHANGE_TICKS){
+	//	DRS_type_change_possible = 0;
+	//}
+}
+
+void TickTimerInit(){
+	// fcpu 16 MHz
+	// preskaler 64
+	// comp 249
+	// 16 000 000 / (64 * (249+1)) = 16000 ---> 1ms
+	TCCR0A = 1<<CS1 | 1 << CS0;
+	TCNT0 = 0;
+	OCR0A = 249;
+	TIFR0 = 0b11; // clearing interrupt flags
+	TIMSK0 = 1<<OCIE0A;
+}
 
 void d_ms(int n) {
 	while(n--) {
@@ -89,53 +122,56 @@ uint16_t pec15(char *data , int len)
 
 void SPI_MasterInit(void)
 {
-	/* Set MOSI and SCK output, all others input */
-	DDRB = (1<<DDB2)|(1<<DDB1)|(1<<DDB5)|(1<<DDB0);
-	/* Enable SPI, Master, set clock rate fck/16 */
-	SPSR = (0<<SPI2X);
-	SPCR = (1<<SPE)|(0<<DORD)|(1<<MSTR)|(1<<CPOL)|(1<<CPHA)|(0<<SPR1)|(1<<SPR0);
+  /* Set MOSI and SCK output, all others input */
+  DDRB = (1<<DDB2)|(1<<DDB1)|(1<<DDB5)|(1<<DDB0)|(1<<DDB6);
+  DDRE = (1<<DDE2);
+  /* Enable SPI, Master, set clock rate fck/16 */
+  SPSR = (0<<SPI2X);
+  SPCR = (1<<SPE)|(0<<DORD)|(1<<MSTR)|(1<<CPOL)|(1<<CPHA)|(0<<SPR1)|(1<<SPR0);
 }
+
 void SPI_Transmit(int8_t  data)  // Byte to be written to SPI port
 {
-	SPDR = data;                  //! 1) Start the SPI transfer
-	while (!(SPSR & _BV(SPIF)));  //! 2) Wait until transfer complete
+  SPDR = data;                  //! 1) Start the SPI transfer
+  while (!(SPSR & (1<<SPIF)));  //! 2) Wait until transfer complete
 }
 
 int8_t SPI_Read(int8_t  data) //!The data byte to be written
 {
-	SPDR = data;                  //! 1) Start the SPI transfer
-	while (!(SPSR & _BV(SPIF)));  //! 2) Wait until transfer complete
-	return SPDR;                  //! 3) Return the data read
+  SPDR = data;                  //! 1) Start the SPI transfer
+  while (!(SPSR & (1<<SPIF))); //! 2) Wait until transfer complete
+  return SPDR;                  //! 3) Return the data read
 }
 
-void SPI_TransmitArray(uint8_t data[], //Array of bytes to be written on the SPI port
-uint8_t len, // Option: Number of bytes to be written on the SPI port
-int transmitDelay)
-{
-	for (uint8_t i = 0; i < len; i++)
-	{
-		SPI_Transmit((int8_t)data[i]);
-		d_ms(transmitDelay);
-	}
-}
-void SPI_TransmitRead(uint8_t tx_Data[],//array of data to be written on SPI port
-uint8_t tx_len, //length of the tx data arry
-uint8_t *rx_data,//Input: array that will store the data read by the SPI port
-uint8_t rx_len, //Option: number of bytes to be read from the SPI port
-int transmitDelay
+void SPI_TransmitArray(uint8_t data[],  //Array of bytes to be written on the SPI port
+uint8_t len                          // Option: Number of bytes to be written on the SPI port
+//int transmitDelay
 )
 {
-	for (uint8_t i = 0; i < tx_len; i++)
-	{
-		SPI_Transmit(tx_Data[i]);
-		d_ms(transmitDelay);
-	}
+  for (uint8_t i = 0; i < len; i++)
+  {
+    SPI_Transmit((int8_t)data[i]);
+    //d_ms(transmitDelay);
+  }
+}
+void SPI_TransmitRead(uint8_t *tx_Data,//array of data to be written on SPI port
+uint8_t tx_len, //length of the tx data array
+uint8_t *rx_data//Input: array that will store the data read by the SPI port
+//uint8_t rx_len, //Option: number of bytes to be read from the SPI port
+//int transmitDelay
+)
+{
+  /*for (uint8_t i = 0; i < tx_len; i++)
+  {
+    SPI_Transmit(tx_Data[i]);
+    d_ms(transmitDelay);
+  }*/
 
-	for (uint8_t i = 0; i < rx_len; i++)
-	{
-		rx_data[i] = (uint8_t)SPI_Read(0xFF);
-		d_ms(transmitDelay);
-	}
+  for (uint8_t i = 0; i < tx_len; i++)
+  {
+    rx_data[i] = (uint8_t)SPI_Read(tx_Data[i]);
+    //d_ms(transmitDelay);
+  }
 
 }
 
@@ -145,23 +181,25 @@ void ltc_wakeup()
 
 	//digitalWrite(SPI_CS_PIN, RESET);
 	PORTB^=1<<DDB0;
-	SPI_TransmitArray(tab, 2, 1);
+	SPI_TransmitArray(tab, 2);
+	PORTB|=1<<DDB0;
 	//digitalWrite(SPI_CS_PIN, SET);
-	PORTB^=1<<DDB0;
+	
 }
 
 void ltc_start_cell_adc()
 {
 	uint8_t tab[12];
 	uint16_t pec;
+	uint16_t cmd = (1<<15) | 0x01;
 	// configuration
-	tab[0] = 0x00;
-	tab[1] = 0x01;
+	tab[0] = (cmd>>8);
+	tab[1] = cmd;
 	pec = pec15((char*)tab, 2);
 	tab[2] = pec >> 8;
 	tab[3] = pec;
 	//
-	tab[4] = ltc_config[0];//0xFC | 0b000; //ltc_config[0];
+	tab[4] = ltc_config[0];//0xFC | 0b000; 
 	tab[5] = ltc_config[1];
 	tab[6] = ltc_config[2];
 	tab[7] = ltc_config[3];
@@ -175,14 +213,14 @@ void ltc_start_cell_adc()
 
 	//digitalWrite(SPI_CS_PIN, RESET);
 	PORTB^=1<<DDB0;
-	SPI_TransmitArray(tab, 12, 100);
+	SPI_TransmitArray(tab, 12);
 	//digitalWrite(SPI_CS_PIN, SET);
-	PORTB^=1<<DDB0;
+	PORTB|=1<<DDB0;
 
 	// adc conversion
 	memset(tab, 0, 12);
 
-	uint16_t cmd = 0b1001100000 | (0b00 << 7);
+	cmd = 0b1001100000 | (0b00 << 7);
 	tab[0] = cmd>>8;
 	tab[1] = cmd;
 	pec = pec15((char*)tab, 2);
@@ -193,8 +231,8 @@ void ltc_start_cell_adc()
 
 	//digitalWrite(SPI_CS_PIN, RESET);
 	PORTB^=1<<DDB0;
-	SPI_TransmitArray(tab, 4, 100);
-	PORTB^=1<<DDB0;
+	SPI_TransmitArray(tab, 4);
+	PORTB|=1<<DDB0;
 	//digitalWrite(SPI_CS_PIN, SET);
 
 }
@@ -203,11 +241,11 @@ void ltc_get_adc_values()
 {
 	uint8_t tab[100], rx_tab[100];
 	uint16_t pec;
-
 	// read cell voltage group A
+	uint16_t cmd = (1<<15) | 0b100;
 	memset(tab, 0, 12);
-	tab[0] = 0;
-	tab[1] = 0b100;
+	tab[0] = (cmd>>8);
+	tab[1] = cmd;
 	pec = pec15((char*)tab, 2);
 	tab[2] = pec >> 8;
 	tab[3] = pec;
@@ -216,27 +254,48 @@ void ltc_get_adc_values()
 
 	//digitalWrite(SPI_CS_PIN, RESET);
 	PORTB^=1<<DDB0;
-	SPI_TransmitRead(tab,12,rx_tab,12,100);
-	PORTB^=1<<DDB0;
+	SPI_TransmitRead(tab,12,rx_tab);
+	PORTB|=1<<DDB0;
 	//digitalWrite(SPI_CS_PIN, SET);
 
 	cell_values[0] = (uint16_t)rx_tab[4] | (((uint16_t)rx_tab[5])<<8);
 	cell_values[1] = (uint16_t)rx_tab[6] | (((uint16_t)rx_tab[7])<<8);
-	cell_values[2] = (uint16_t)rx_tab[8] | (((uint16_t)rx_tab[9])<<8);
+	cell_values[2] = (uint16_t)rx_tab[8] | (((uint16_t)rx_tab[9])<<8); //zwarty
+	
+	// read cell voltage group B
+	/*cmd = (1<<15) | 0b110;
+	memset(tab, 0, 12);
+	tab[0] = (cmd>>8);
+	tab[1] = cmd;
+	pec = pec15((char*)tab, 2);
+	tab[2] = pec >> 8;
+	tab[3] = pec;
+
+	//ltc_wakeup();
+
+	PORTB^=1<<DDB0;
+	SPI_TransmitRead(tab,12,rx_tab);
+	PORTB^=1<<DDB0;
+
+	cell_values[3] = (uint16_t)rx_tab[4] | (((uint16_t)rx_tab[5])<<8); //zwarty ?
+	cell_values[4] = (uint16_t)rx_tab[6] | (((uint16_t)rx_tab[7])<<8);
+	cell_values[5] = (uint16_t)rx_tab[8] | (((uint16_t)rx_tab[9])<<8);
+*/
 }
 
 void ltc_start_temp_adc()
 {
 	uint8_t tab[12];
 	uint16_t pec;
+	uint16_t cmd = (1<<15) | 0x01;
 	// configuration
-	tab[0] = 0x00;
-	tab[1] = 0x01;
+	tab[0] = (cmd>>8);
+	tab[1] = cmd;
 	pec = pec15((char*)tab, 2);
 	tab[2] = pec >> 8;
 	tab[3] = pec;
 	//
-	tab[4] = 0xFC | 0b000; //ltc_config[0];
+	tab[4] = ltc_config[0]; //0xFC | 0b000;
 	tab[5] = ltc_config[1];
 	tab[6] = ltc_config[2];
 	tab[7] = ltc_config[3];
@@ -250,14 +309,14 @@ void ltc_start_temp_adc()
 
 	//digitalWrite(SPI_CS_PIN, RESET);
 	PORTB^=1<<DDB0;
-	SPI_TransmitArray(tab, 12, 100);
+	SPI_TransmitArray(tab, 12);
+	PORTB|=1<<DDB0;
 	//digitalWrite(SPI_CS_PIN, SET);
-	PORTB^=1<<DDB0;
 
 	// adc conversion
 	memset(tab, 0, 12);
 
-	uint16_t cmd = 0b10001100000 | (0b00 << 7);
+	cmd = 0b10001100000 | (0b00 << 7);
 	tab[0] = cmd>>8;
 	tab[1] = cmd;
 	pec = pec15((char*)tab, 2);
@@ -266,82 +325,104 @@ void ltc_start_temp_adc()
 
 	//ltc_wakeup();
 
-	//digitalWrite(SPI_CS_PIN, RESET);
 	PORTB^=1<<DDB0;
-	SPI_TransmitArray(tab, 12, 100);
-	//digitalWrite(SPI_CS_PIN, SET);
-	PORTB^=1<<DDB0;
+	SPI_TransmitArray(tab, 12);
+	PORTB|=1<<DDB0;
 }
 
 void ltc_get_temp_values()
 {
 	uint8_t tab[100], rx_tab[100];
 	uint16_t pec;
+	uint16_t cmd = (1<<15) | 0b1100;
 
 	// read gpio voltage group A
 	memset(tab, 0, 12);
-	tab[0] = 0;
-	tab[1] = 0b1100;
+	tab[0] = (cmd>>8);
+	tab[1] = cmd;
 	pec = pec15((char*)tab, 2);
 	tab[2] = pec >> 8;
 	tab[3] = pec;
 
 	ltc_wakeup();
 
-	//digitalWrite(SPI_CS_PIN, RESET);
 	PORTB^=1<<DDB0;
-	SPI_TransmitRead(tab,12,rx_tab,12,100);
-	PORTB^=1<<DDB0;
-	//digitalWrite(SPI_CS_PIN, SET);
+	SPI_TransmitRead(tab,12,rx_tab);
+	PORTB|=1<<DDB0;
 
 	temp_values[0] = (uint16_t)rx_tab[4] | (((uint16_t)rx_tab[5])<<8);
 	temp_values[1] = (uint16_t)rx_tab[6] | (((uint16_t)rx_tab[7])<<8);
 	temp_values[2] = (uint16_t)rx_tab[8] | (((uint16_t)rx_tab[9])<<8);
+	
+	// read gpio voltage group B
+	cmd = (1<<15) | 0b1110;
+	memset(tab, 0, 12);
+	tab[0] = (cmd>>8);
+	tab[1] = cmd;
+	pec = pec15((char*)tab, 2);
+	tab[2] = pec >> 8;
+	tab[3] = pec;
+
+	ltc_wakeup();
+
+	PORTB^=1<<DDB0;
+	SPI_TransmitRead(tab,12,rx_tab);
+	PORTB|=1<<DDB0;
+
+	temp_values[0] = (uint16_t)rx_tab[4] | (((uint16_t)rx_tab[5])<<8);
+	temp_values[1] = (uint16_t)rx_tab[6] | (((uint16_t)rx_tab[7])<<8);
+	temp_values[2] = (uint16_t)rx_tab[8] | (((uint16_t)rx_tab[9])<<8);
+	
+	
 }
 
-float temperature_calculate(uint16_t ltc_value)
+int main(void)
 {
-	float retval = 0.0;
-	for(int i = 1; i < 28; i++)
-	{
-		if(ltc_value >= (uint16_t)temperature_map[i][0])
-		{
-			// approximation
-			retval = temperature_map[i][1] - 5.0 * ((float)ltc_value-temperature_map[i][0])/(temperature_map[i-1][0] - temperature_map[i][0]);
-			break;
-		}
-	}
-	return retval;
-}
-
-
-int main(void){
-	// put your setup code here, to run once:
-
-	SPI_MasterInit();
 	init_PEC15_Table();
-	//DDRB = 0b10111111;
-
-	// put your main code here, to run repeatedly:
-	while(1){
-		
-		//PORTB ^= 0b10111111;
+	SPI_MasterInit();
+	TickTimerInit();
+	/*
+	uint8_t tab[12];
+	uint8_t tab_r[12];
+	uint16_t cmd=(1<<15)|(0b101100);
+	ltc_wakeup();
+	
+	tab[0]=(cmd>>8);
+	tab[1]=cmd;
+	uint16_t pec=pec15(tab,2);
+	tab[2]=(pec>>8);
+	tab[3]=pec;
+	
+	PORTB^=1<<DDB0;
+	SPI_TransmitRead(tab, 12, tab_r);
+	PORTB|=1<<DDB0;
+	
+	uint64_t sid;
+	memcpy(&sid,&tab_r[4],6);
+	if(sid!=0x0){
+		PORTB |= 1 << DDB6;
+	}*/
+	
+    /* Replace with your application code */
+    while (1) 
+    {
 		PORTB ^= 1 << DDB5;
 		d_ms(500);
-		//PORTB ^= 0b10111111;
-
-		//_delay_ms(500);
-		ltc_start_cell_adc();
-		d_ms(200);
 		
-		ltc_get_adc_values();
-
-		ltc_start_temp_adc();
-		
-		d_ms(200);
-		ltc_get_temp_values();
-	}
-
-	
-
+			/*
+			PORTB^=1<<DDB0;
+			SPI_TransmitRead(tab, 12, tab_r);
+			PORTB|=1<<DDB0;
+			
+			uint64_t sid;
+			memcpy(&sid,&tab_r[4],6);
+			if(sid!=0xFFFFFFFFFFFF){
+				PORTB ^= 1 << DDB6;
+			}*/
+			
+			ltc_start_cell_adc();
+			d_ms(20);
+			ltc_get_adc_values();
+    }
 }
+
