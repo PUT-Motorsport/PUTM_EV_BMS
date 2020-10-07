3b1cb422b3ce8a9aea6b5e77ff7a45659bba210b
Morgaliel <maksymilian.jaruga@student.put.poznan.pl>
2020-10-07 20:34:23 +0200

    CAN work part 2
    CAN ready to test. Don't know if it's working already.
    Some visual changes as well.

--- main.c
+++ main.c
@@ -16,46 +16,11 @@
 const can_filter_t filtersetup = {0,0,{0}};
 
 //	timers
-volatile uint64_t timer_counter = 0;
-volatile uint64_t timer_counter_2 = 0;
-volatile uint64_t timer_counter_3 = 0;
+volatile uint64_t timerCounter = 0;
+volatile uint64_t timerCounter_2 = 0;
+volatile uint64_t timerCounter_3 = 0;
 
-/*
-float temperature_map_2[29][2] = {
-	//		ltc value ,  temperature *C
-	27190	,	-20	,
-	26380	,	-15	,
-	25400	,	-10	,
-	24260	,	-5	,
-	22960	,	0	,
-	21520	,	5	,
-	19960	,	10	,
-	18330	,	15	,
-	16660	,	20	,
-	15000	,	25	,
-	13380	,	30	,
-	11850	,	35	,
-	10420	,	40	,
-	9120	,	45	,
-	7940	,	50	,
-	6890	,	55	,
-	5970	,	60	,
-	5170	,	65	,
-	4470	,	70	,
-	3860	,	75	,
-	3350	,	80	,
-	2900	,	85	,
-	2520	,	90	,
-	2190	,	95	,
-	1910	,	100	,
-	1660	,	105	,
-	1450	,	110	,
-	1270	,	115
-
-};
-*/
-
-float temperature_map[29][2] = {
+float temperatureMap[29][2] = {
 	//    ltc value ,  temperature *C
 	48656 , -20 ,
 	45988 , -15 ,
@@ -87,126 +52,109 @@ float temperature_map[29][2] = {
 	2214  , 115
 };
 
-uint8_t ltc_config[6] = {0xFC, (1874 & 0xff), (1874>>4)|(2625<<4), (2625>>4), 0, 0};
-uint8_t acu_state[8]={0,0,0,0,0,0,0,0};
-uint8_t cell_values[10];
-uint8_t cell_values_sum;
-uint8_t cell_values_low;
-uint8_t cell_values_high;
-uint8_t temp_values[10];
-uint8_t temp_values_2[10];
-uint8_t temp_values_avr;
-uint8_t temp_values_high;
-uint8_t reference2_value;
-uint8_t reference2_value_2;
-uint8_t can_flag=0;
-float temp_values_float[10];
-float temp_values_2_float[10];
+uint8_t ltcConfig[6] = {0xFC, (1874 & 0xff), (1874>>4)|(2625<<4), (2625>>4), 0, 0};
+uint8_t acuState[8]={0,0,0,0,0,0,0,0};
+uint16_t cellValues[10];
+uint16_t cellValuesSum;
+uint16_t cellValuesLow;
+uint16_t cellValuesHigh;
+uint16_t tempValues[10];
+uint16_t tempValues_2[10];
+uint16_t tempValuesAvr;
+uint16_t tempValuesHigh;
+uint16_t reference2_value;
+uint16_t reference2_value_2;
+uint8_t canFlag=0;
+float tempValuesFloat[10];
 
 ISR(TIMER0_COMP_vect)
 {
 	TCNT0 = 0;
-	//timer_counter++;
+	
+	timerCounter_3++;
+	if((timerCounter_3%20)==0){
+		canFlag=1;
+	}
 	
 	
-	if((cell_values[0]<35000 || cell_values[0]>42300) || (cell_values[1]<35000 || cell_values[1]>42300) || (cell_values[3]<35000 || cell_values[3]>42300) || (cell_values[4]<35000 || cell_values[4]>42300)){
-		acu_state|=0b1;
-		if((cell_values[0]<35000 || cell_values[0]>42300)){
-			if(cell_values[0]<35000){
-				acu_state|=0b1000;
+	if((cellValues[0] < 35000 || cellValues[0] > 42300) || (cellValues[1] < 35000 || cellValues[1] > 42300) || (cellValues[3] < 35000 || cellValues[3] > 42300) || (cellValues[4] < 35000 || cellValues[4] > 42300)){
+		acuState |= 0b1;
+		if((cellValues[0] < 35000 || cellValues[0] > 42300)){
+			if(cellValues[0] < 35000){
+				acuState |= 0b1000;
 			}else{
-				acu_state|=0b10000;
+				acuState |= 0b10000;
 			}
 		}
-		if((cell_values[1]<35000 || cell_values[1]>42300)){
-			acu_state|=0b10;
-			if(cell_values[1]<35000){
-				acu_state|=0b1000;
+		if((cellValues[1] < 35000 || cellValues[1] > 42300)){
+			acuState |= 0b10;
+			if(cellValues[1] < 35000){
+				acuState |= 0b1000;
 				}else{
-				acu_state|=0b10000;
+				acuState |= 0b10000;
 			}
 		}
-		if((cell_values[2]<35000 || cell_values[2]>42300)){
-			acu_state|=0b100;
-			if(cell_values[2]<35000){
-				acu_state|=0b1000;
+		if((cellValues[2] < 35000 || cellValues[2] > 42300)){
+			acuState |= 0b100;
+			if(cellValues[2] < 35000){
+				acuState |= 0b1000;
 				}else{
-				acu_state|=0b10000;
+				acuState |= 0b10000;
 			}
 		}
-		if((cell_values[3]<35000 || cell_values[3]>42300)){
-			acu_state|=0b110;
-			if(cell_values[3]<35000){
-				acu_state|=0b1000;
+		if((cellValues[3] < 35000 || cellValues[3] > 42300)){
+			acuState |= 0b110;
+			if(cellValues[3] < 35000){
+				acuState |= 0b1000;
 				}else{
-				acu_state|=0b10000;
+				acuState |= 0b10000;
 			}
 		}
 		
-		timer_counter++;
+		timerCounter++;
 	}
 	
-	if((cell_values[0]>=42000 && cell_values[0]<=42300) && (cell_values[1]>=35000 && cell_values[1]<=42300) && (cell_values[3]>=35000 && cell_values[3]<=42300) && (cell_values[4]>=35000 && cell_values[4]<=42300)){
-		acu_state&=0b0;
-		timer_counter = 0;
+	if((cellValues[0] >= 42000 && cellValues[0] <= 42300) && (cellValues[1] >= 35000 && cellValues[1] <= 42300) && (cellValues[3] >= 35000 && cellValues[3] <= 42300) && (cellValues[4] >= 35000 && cellValues[4] <= 42300)){
+		acuState &= 0b0;
+		timerCounter = 0;
 	}
 	
-	if(temp_values_float[1]>50 || temp_values_float[2]>50 || temp_values_float[3]>50 || temp_values_float[4]>50){
-		acu_state|=0b1;
-		if(temp_values_float[1]>50){
-			acu_state|=0b100000;
+	if(tempValuesFloat[1] > 50 || tempValuesFloat[2] > 50 || tempValuesFloat[3] > 50 || tempValuesFloat[4] > 50){
+		acuState |= 0b1;
+		if(tempValuesFloat[1] > 50){
+			acuState |= 0b100000;
 		}
-		if(temp_values_float[2]>50){
-			acu_state|=0b10;
-			acu_state|=0b100000;
+		if(tempValuesFloat[2] > 50){
+			acuState |= 0b10;
+			acuState |= 0b100000;
 		}
-		if(temp_values_float[3]>50){
-			acu_state|=0b100;
-			acu_state|=0b100000;
+		if(tempValuesFloat[3] > 50){
+			acuState |= 0b100;
+			acuState |= 0b100000;
 		}
-		if(temp_values_float[4]>50){
-			acu_state|=0b110;
-			acu_state|=0b100000;
+		if(tempValuesFloat[4] > 50){
+			acuState |= 0b110;
+			acuState |= 0b100000;
 		}
-		timer_counter_2=timer_counter_2+1;
+		timerCounter_2++;
 	}
 	
-	if(temp_values_float[1]<=50 && temp_values_float[2]<=50 && temp_values_float[3]<=50 && temp_values_float[4]<=50){
-		acu_state&=0b0;
-		timer_counter_2 = 0;
+	if(tempValuesFloat[1] <= 50 && tempValuesFloat[2] <= 50 && tempValuesFloat[3] <= 50 && tempValuesFloat[4] <= 50){
+		acuState &= 0b0;
+		timerCounter_2 = 0;
 	}
 	
-	if(timer_counter >= 1000){
+	if(timerCounter >= 1000){
 		PORTE &= 0 << DDE2;
 	}
 	
-	if(timer_counter_2 >= 1000){
+	if(timerCounter_2 >= 1000){
 		PORTE &= 0 << DDE2;
 	}
 	
-}
-
-ISR(TIMER0_COMP_vect)
-{
-	timer_counter_3++;
-	if((timer_counter_3%20)==0){
-		can_flag=1;
-	}
 }
 
 void TickTimerInit(){
-	// fcpu 16 MHz
-	// preskaler 64
-	// comp 249
-	// 16 000 000 / (64 * (249+1)) = 16000 ---> 1ms
-	TCCR0A = 1<<CS1 | 1 << CS0;
-	TCNT0 = 0;
-	OCR0A = 249;
-	TIFR0 = 0b11; // clearing interrupt flags
-	TIMSK0 = 1<<OCIE0A;
-}
-
-void TickTimerInit_2(){
 	// fcpu 16 MHz
 	// preskaler 64
 	// comp 249
@@ -305,7 +253,7 @@ uint8_t *rx_data//Input: array that will store the data read by the SPI port
 
 }
 
-void ltc_wakeup()
+void LTC_Wakeup()
 {
 	uint8_t tab[2] = {0xFF};
 
@@ -315,7 +263,7 @@ void ltc_wakeup()
 	
 }
 
-void ltc_start_cell_adc()
+void LTC_StartCellADC()
 {
 	uint8_t tab[12];
 	uint16_t pec;
@@ -327,17 +275,17 @@ void ltc_start_cell_adc()
 	tab[2] = pec >> 8;
 	tab[3] = pec;
 	
-	tab[4] = ltc_config[0];
-	tab[5] = ltc_config[1];
-	tab[6] = ltc_config[2];
-	tab[7] = ltc_config[3];
-	tab[8] = ltc_config[4];
-	tab[9] = ltc_config[5];
+	tab[4] = ltcConfig[0];
+	tab[5] = ltcConfig[1];
+	tab[6] = ltcConfig[2];
+	tab[7] = ltcConfig[3];
+	tab[8] = ltcConfig[4];
+	tab[9] = ltcConfig[5];
 	pec = pec15((char*)&tab[4], 6);
 	tab[10] = pec >> 8;
 	tab[11] = pec;
 
-	ltc_wakeup();
+	LTC_Wakeup();
 
 	PORTB ^= 1<<DDB0;
 	SPI_TransmitArray(tab, 12);
@@ -353,7 +301,7 @@ void ltc_start_cell_adc()
 	tab[2] = pec >> 8;
 	tab[3] = pec;
 
-	//ltc_wakeup();
+	//LTC_Wakeup();
 
 	PORTB ^= 1<<DDB0;
 	SPI_TransmitArray(tab, 4);
@@ -361,10 +309,11 @@ void ltc_start_cell_adc()
 
 }
 
-void ltc_get_adc_values()
+void LTC_GetValuesADC()
 {
 	uint8_t tab[100], rx_tab[100];
 	uint16_t pec;
+	
 	// read cell voltage group A
 	uint16_t cmd = (1<<15) | 0b100;
 	memset(tab, 0, 12);
@@ -374,15 +323,15 @@ void ltc_get_adc_values()
 	tab[2] = pec >> 8;
 	tab[3] = pec;
 
-	ltc_wakeup();
+	LTC_Wakeup();
 
 	PORTB ^= 1<<DDB0;
 	SPI_TransmitRead(tab,12,rx_tab);
 	PORTB |= 1<<DDB0;
 
-	cell_values[0] = (uint16_t)rx_tab[4] | (((uint16_t)rx_tab[5])<<8);
-	cell_values[1] = (uint16_t)rx_tab[6] | (((uint16_t)rx_tab[7])<<8);
-	cell_values[2] = (uint16_t)rx_tab[8] | (((uint16_t)rx_tab[9])<<8); //zwarty
+	cellValues[0] = (uint16_t)rx_tab[4] | (((uint16_t)rx_tab[5])<<8);
+	cellValues[1] = (uint16_t)rx_tab[6] | (((uint16_t)rx_tab[7])<<8);
+	cellValues[2] = (uint16_t)rx_tab[8] | (((uint16_t)rx_tab[9])<<8); //short circuit
 	
 	// read cell voltage group B
 	cmd = (1<<15) | 0b110;
@@ -393,51 +342,55 @@ void ltc_get_adc_values()
 	tab[2] = pec >> 8;
 	tab[3] = pec;
 
-	//ltc_wakeup();
+	//LTC_Wakeup();
 
 	PORTB ^= 1<<DDB0;
 	SPI_TransmitRead(tab,12,rx_tab);
 	PORTB ^= 1<<DDB0;
 
-	cell_values[3] = (uint16_t)rx_tab[4] | (((uint16_t)rx_tab[5])<<8);
-	cell_values[4] = (uint16_t)rx_tab[6] | (((uint16_t)rx_tab[7])<<8);
-	cell_values[5] = (uint16_t)rx_tab[8] | (((uint16_t)rx_tab[9])<<8); //zwarty
-	cell_values_sum = cell_values[0];
-	cell_values_low = cell_values[0];
-	cell_values_high = cell_values[0];
+	cellValues[3] = (uint16_t)rx_tab[4] | (((uint16_t)rx_tab[5])<<8);
+	cellValues[4] = (uint16_t)rx_tab[6] | (((uint16_t)rx_tab[7])<<8);
+	cellValues[5] = (uint16_t)rx_tab[8] | (((uint16_t)rx_tab[9])<<8); //short circuit
 	
+	cellValuesSum  = cellValues[0];
+	cellValuesLow  = cellValues[0];
+	cellValuesHigh = cellValues[0];
+	
+	//cell calculations
 	for(int i = 1; i < 5; i++)
 	{
 		if(i!=2){
-			cell_values_sum = cell_values_sum+cell_values[i];
-			if(cell_values_low > cell_values[i]){
-				cell_values_low = cell_values[i];
+			cellValuesSum = cellValuesSum+cellValues[i];
+			if(cellValuesLow > cellValues[i]){
+				cellValuesLow = cellValues[i];
 			}
-			if(cell_values_high < cell_values[i]){
-				cell_values_high = cell_values[i];
+			if(cellValuesHigh < cellValues[i]){
+				cellValuesHigh = cellValues[i];
 			}
 		}
 	}
-	
+	cellValuesSum  = cellValuesSum/1000;
+	cellValuesHigh = cellValuesHigh/1000;
+	cellValuesLow  = cellValuesLow/1000;
 	
 }
 
-float temperature_calculate(uint16_t ltc_value)
+float tempCalculate(uint16_t ltc_value)
 {
 	float retval = 0.0;
 	for(int i = 1; i < 28; i++)
 	{
-		if(ltc_value >= (uint16_t)temperature_map[i][0])
+		if(ltc_value >= (uint16_t)temperatureMap[i][0])
 		{
 			// approximation
-			retval = temperature_map[i][1] - 5.0 * ((float)ltc_value-temperature_map[i][0])/(temperature_map[i-1][0] - temperature_map[i][0]);
+			retval = temperatureMap[i][1] - 5.0 * ((float)ltc_value-temperatureMap[i][0]) / (temperatureMap[i-1][0] - temperatureMap[i][0]);
 			break;
 		}
 	}
 	return retval;
 }
 
-void ltc_start_temp_adc()
+void LTC_StartTempValues()
 {
 	uint8_t tab[12];
 	uint16_t pec;
@@ -449,17 +402,17 @@ void ltc_start_temp_adc()
 	tab[2] = pec >> 8;
 	tab[3] = pec;
 	
-	tab[4] = ltc_config[0];
-	tab[5] = ltc_config[1];
-	tab[6] = ltc_config[2];
-	tab[7] = ltc_config[3];
-	tab[8] = ltc_config[4];
-	tab[9] = ltc_config[5];
+	tab[4] = ltcConfig[0];
+	tab[5] = ltcConfig[1];
+	tab[6] = ltcConfig[2];
+	tab[7] = ltcConfig[3];
+	tab[8] = ltcConfig[4];
+	tab[9] = ltcConfig[5];
 	pec = pec15((char*)&tab[4], 6);
 	tab[10] = pec >> 8;
 	tab[11] = pec;
 
-	ltc_wakeup();
+	LTC_Wakeup();
 
 	PORTB ^= 1<<DDB0;
 	SPI_TransmitArray(tab, 12);
@@ -475,7 +428,7 @@ void ltc_start_temp_adc()
 	tab[2] = pec >> 8;
 	tab[3] = pec;
 
-	//ltc_wakeup();
+	//LTC_Wakeup();
 
 	PORTB ^= 1<<DDB0;
 	SPI_TransmitArray(tab, 12);
@@ -483,7 +436,7 @@ void ltc_start_temp_adc()
 }
 
 
-void ltc_get_temp_values()
+void LTC_GetTempValues()
 {
 	uint8_t tab[100], rx_tab[100];
 	uint16_t pec;
@@ -496,19 +449,15 @@ void ltc_get_temp_values()
 	tab[2] = pec >> 8;
 	tab[3] = pec;
 
-	ltc_wakeup();
+	LTC_Wakeup();
 	
 	PORTB ^= 1<<DDB0;
 	SPI_TransmitRead(tab,20,rx_tab);
 	PORTB |= 1<<DDB0;
 
-	temp_values[0] = (uint16_t)rx_tab[4] | (((uint16_t)rx_tab[5])<<8);
-	temp_values[1] = (uint16_t)rx_tab[6] | (((uint16_t)rx_tab[7])<<8);
-	temp_values[2] = (uint16_t)rx_tab[8] | (((uint16_t)rx_tab[9])<<8);
-
-	/*temp_values_2[0] = (uint16_t)rx_tab[12] | (((uint16_t)rx_tab[13])<<8);
-	temp_values_2[1] = (uint16_t)rx_tab[14] | (((uint16_t)rx_tab[15])<<8);
-	temp_values_2[2] = (uint16_t)rx_tab[16] | (((uint16_t)rx_tab[17])<<8);*/
+	tempValues[0] = (uint16_t)rx_tab[4] | (((uint16_t)rx_tab[5])<<8);
+	tempValues[1] = (uint16_t)rx_tab[6] | (((uint16_t)rx_tab[7])<<8);
+	tempValues[2] = (uint16_t)rx_tab[8] | (((uint16_t)rx_tab[9])<<8);
 
 	// read cell voltage group B
 	memset(tab, 0, 12);
@@ -518,35 +467,31 @@ void ltc_get_temp_values()
 	tab[2] = pec >> 8;
 	tab[3] = pec;
 
-	//ltc_wakeup();
+	//LTC_Wakeup();
 
 	PORTB ^= 1<<DDB0;
 	SPI_TransmitRead(tab,20,rx_tab);
 	PORTB |= 1<<DDB0;
 
-	temp_values[3] = (uint16_t)rx_tab[4] | (((uint16_t)rx_tab[5])<<8);
-	temp_values[4] = (uint16_t)rx_tab[6] | (((uint16_t)rx_tab[7])<<8);
+	tempValues[3] = (uint16_t)rx_tab[4] | (((uint16_t)rx_tab[5])<<8);
+	tempValues[4] = (uint16_t)rx_tab[6] | (((uint16_t)rx_tab[7])<<8);
 	reference2_value = (uint16_t)rx_tab[8] | (((uint16_t)rx_tab[9])<<8);
 	
-	temp_values_avr = 0;
+	tempValuesAvr = 0;
 
-	/*temp_values_2[3] = (uint16_t)rx_tab[12] | (((uint16_t)rx_tab[13])<<8);
-	temp_values_2[4] = (uint16_t)rx_tab[14] | (((uint16_t)rx_tab[15])<<8);
-	reference2_value_2 = (uint16_t)rx_tab[16] | (((uint16_t)rx_tab[17])<<8);*/
-	
+	//temp calculations
 	for(int i = 1; i < 5; i++)
 	{
-		temp_values_float[i] = temperature_calculate(temp_values[i]);
-		temp_values_2_float[i] = temperature_calculate(temp_values_2[i]);
+		tempValuesFloat[i] = tempCalculate(tempValues[i]);
 		
-		temp_values_avr = temp_values_avr + temp_values_float[i];
+		tempValuesAvr = tempValuesAvr + tempValuesFloat[i];
 	}
-	temp_values_avr = temp_values_avr/4;
+	tempValuesAvr = tempValuesAvr/4;
 	
-	temp_values_high=temp_values_float[1];
+	tempValuesHigh=tempValuesFloat[1];
 	for(int i = 2; i < 5; i++){
-		if(temp_values_high < temp_values_float[i]){
-			temp_values_high = temp_values_float[i];
+		if(tempValuesHigh < tempValuesFloat[i]){
+			tempValuesHigh = tempValuesFloat[i];
 		}
 	}
 }
@@ -557,7 +502,6 @@ int main(void)
 	init_PEC15_Table();
 	SPI_MasterInit();
 	TickTimerInit();
-	TickTimerInit_2();
 	sei();
 	
 	can_init(BITRATE_500_KBPS);
@@ -568,7 +512,7 @@ int main(void)
 	uint8_t tab[12];
 	uint8_t tab_r[12];
 	uint16_t cmd=(1<<15)|(0b101100);
-	ltc_wakeup();
+	LTC_Wakeup();
 	
 	tab[0]=(cmd>>8);
 	tab[1]=cmd;
@@ -602,16 +546,34 @@ int main(void)
 				PORTB ^= 1 << DDB6;
 			}*/
 			
-			ltc_start_cell_adc();
+			LTC_StartCellADC();
 			d_ms(20);
-			ltc_get_adc_values();
+			LTC_GetValuesADC();
 			
-			ltc_start_temp_adc();
+			LTC_StartTempValues();
 			d_ms(20);
-			ltc_get_temp_values();
+			LTC_GetTempValues();
+			
+			if(canFlag == 1){
+				canFlag = 0;
+				static can_t txMessage;
+				
+				txMessage.id = 0x0B;
+				txMessage.flags.rtr = 0;
+				txMessage.length = 8;
+				
+				txMessage.data[0] = cellValuesSum;
+				txMessage.data[1] = acuState;
+				txMessage.data[2] = tempValuesAvr;
+				txMessage.data[3] = tempValuesHigh;
+				txMessage.data[4] = cellValuesLow;
+				txMessage.data[5] = cellValuesHigh;
+				
+				can_send_message(&txMessage);
+			}
 			
 			
-			if(temp_values_float[4]>30){
+			if(tempValuesFloat[4] > 30){
 			PORTB ^= 1 << DDB6;
 			}
     }
